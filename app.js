ae2d44d94af4ee6efd55f6e5e2cd81b8acae034f
Lee <winterfive@users.noreply.github.com>
2019-03-26 15:28:21 -0400

    Created runTimer(), reset(), displayCountdown()

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let currentBreak = 0;
 let isPaused = false;
 let isSessionRunning = false;
 let isBreakRunning = false;
+let startSession;
+let startBreak;
 
 class App extends React.Component {
   constructor(props) {
@@ -14,43 +16,16 @@ class App extends React.Component {
     this.state = {
       sessionLength: 25,
       breakLength: 5,
-      startStoplabel: "Start",
+      startStopLabel: "Start",
       timeLeft: "00:00",
-      timeLabel: "Session: "
+      timeLabel: "Session Time: "
     };
   }
-
-  // logic
-  /*
-  function displayCountdown(amount) {
-      if (amount < 600) {
-        // if less than 10 minutes
-        if (amount % 60 > 9) {
-          // 10 seconds or more
-          $("#time-left").html("0" + Math.floor(amount / 60) + ":" + amount % 60);
-        } else {
-          // 9 seconds or less
-          $("#time-left").html("0" + Math.floor(amount / 60) + ":" + "0" + amount % 60);
-        }
-      } else {
-        // if more than 10 minutes
-        if (amount % 60 > 9) {
-          // 10 seconds or more
-          $("#time-left").html(Math.floor(amount / 60) + ":" + amount % 60);
-        } else {
-          // 9 seconds or less
-          $("#time-left").html(Math.floor(amount / 60) + ":" + "0" + amount % 60);
-        }
-      }
-    }
-  });
-  */
 
   // Updates session and break count prop
   // string, string => void
   updateCount(x, y) {
     let z = 0;
-
     // update sessionLength
     if (x === "session") {
       z = this.state.sessionLength;
@@ -97,13 +72,144 @@ class App extends React.Component {
       });
     }
   }
-  
+
   // Starts and stops timer
   // void -> void
-  runTimer() {    
-    if(isPaused) {
-      
+  controlTimer() {
+    // pause button
+    if ((!isPaused && isSessionRunning) || (!isPaused && isBreakRunning)) {
+      isPaused = true;
+      if (isSessionRunning) {
+        clearInterval(startSession);
+        return;
+      }
+      if (isBreakRunning) {
+        clearInterval(startBreak);
+        return;
+      }
     }
+
+    // restarting session or break
+    if (isPaused) {
+      if (isSessionRunning) {
+        //restart session
+        startSession = setInterval(function() {
+          runTimer();
+        }, 1000);
+      }
+
+      if (isBreakRunning) {
+        //restart break
+        startBreak = setInterval(function() {
+          runTimer();
+        }, 1000);
+      }
+      isPaused = false;
+      this.setState({
+        startStop: "Stop"
+      });
+    }
+
+    // Starting a new session
+    if (!isSessionRunning && !isBreakRunning) {
+      currentSession = this.state.sessionLength * 60;
+      isSessionRunning = true;
+      startSession = setInterval(function() {
+        runTimer();
+      }, 1000);
+      this.setState({
+        startStop: "Stop"
+      });
+    }
+  }
+
+  runTimer() {
+    if (isSessionRunning) {
+      sessCount -= 1;
+      displayCountdown(currentSession);
+    }
+
+    if (isBreakRunning) {
+      breakCount -= 1;
+      displayCountdown(currentBreak);
+    }
+
+    if (currentSession === 0 || currentBreak === 0) {
+      playAudio();
+      if (isSessionRunning) {
+        // stop session, start break
+        clearInterval(startSession);
+        currentSession = this.state.sessionLength * 60;
+        currentBreak = this.state.sessionLength * 60;
+        this.setState({
+          timeLabel: "Break Time: "
+        });
+        startBreak = setInterval(function() {
+          runTimer();
+        }, 1000);
+        isSessionRunning = false;
+        isBreakRunning = true;
+      } else {
+        // stop break, start session
+        clearInterval(startBreak);
+        currentSession = this.state.sessionLength * 60;
+        currentBreak = this.state.sessionLength * 60;
+        this.setState({
+          timeLabel: "Session Time: "
+        });
+        startSession = setInterval(function() {
+          runTimer();
+        }, 1000);
+        isSessionRunning = true;
+        isBreakRunning = false;
+      }
+    }
+  }
+  
+  displayCountdown(amount) {
+    if (amount < 600) {
+      // if less than 10 minutes
+      if (amount % 60 > 9) {
+        // 10 seconds or more
+        this.setState({
+          timeLeft: "0" + Math.floor(amount / 60) + ":" + amount % 60
+        })
+      } else {
+        // 9 seconds or less
+        this.setState({
+          timeLeft: "0" + Math.floor(amount / 60) + ":" + "0" + amount % 60
+        })
+      }
+    } else {
+      // if more than 10 minutes
+      if (amount % 60 > 9) {
+        // 10 seconds or more
+
+        this.setState({
+          timeLeft: Math.floor(amount / 60) + ":" + amount % 60
+        })
+      } else {
+        // 9 seconds or less
+        this.setState({
+          timeLeft: Math.floor(amount / 60) + ":" + "0" + amount % 60
+        })
+      }
+    }
+  }
+  
+  reset() {
+    this.setState({
+      sessionLength: 25,
+      breakLength: 5,
+      startStopLabel: "Start",
+      timeLeft: "00:00",
+      timeLabel: "Session Time: "
+    })
+    let currentSession = 0;
+    let currentBreak = 0;
+    let isPaused = false;
+    let isSessionRunning = false;
+    let isBreakRunning = false;
   }
 
   render() {
@@ -157,14 +263,20 @@ class App extends React.Component {
           </div>
           <div id="timerDiv" class="centerRow">
             <h2 id="timer-label">
-              {this.state.timeLabel}<span id="time-left">{this.state.timeLeft}</span>
+              {this.state.timeLabel}
+              <span id="time-left">{this.state.timeLeft}</span>
             </h2>
           </div>
           <div class="centerRow">
-            <a href="#" id="start_stop" class="btn" onClick={() => this.runTimer}>
-              {this.state.startStoplabel}
+            <a
+              href="#"
+              id="start_stop"
+              class="btn"
+              onClick={() => this.controlTimer()}
+            >
+              {this.state.startStopLabel}
             </a>
-            <a href="#" id="reset" class="btn">
+            <a href="#" id="reset" class="btn" onClick={() => this.reset()}>
               Reset
             </a>
           </div>
